d2887ce2de4f18dc39eeabca69e023a0197f3653
danielgino <danielgino3@Gmail.com>
2025-05-04 16:35:06 +0300

    Strict Mode on

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 Modal.setAppElement('#root');
 root.render(
-   //  <React.StrictMode>
+    <React.StrictMode>
       <ThemeProvider>
           <UserProvider>
           <App />
           </UserProvider>
       </ThemeProvider>
- // </React.StrictMode>  ///@@@@@@@@BACK IN PRODUCTION@@@@@@@@@@@@@@@@@@@//
+ </React.StrictMode>  ///@@@@@@@@BACK IN PRODUCTION@@@@@@@@@@@@@@@@@@@//
 );
 
 // If you want to start measuring performance in your app, pass a function
